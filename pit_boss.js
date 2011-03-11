f537e477a53be0f155393e25a5e7e20bce68da1e
Derek Croft <derek@derekcroft.com>
2011-03-10 22:56:42 -0500

    Add methods to dispatch body processing.

--- pit_boss.js
+++ pit_boss.js
@@ -10,24 +10,38 @@ var html = function(url) {
   return '<html>' + head() + body(url) + '</html>';
 };
 
+
 var head = function() {
   return '<head>' + title() + '</head>';
 };
+
 
 var title = function() {
   return '<title>Serving Craps since 2011</title>';
 };
 
+
 var body = function(url) {
-  var results = '<body>';
-  if (url === '/tables') {
-    results += "<h1>Tables</h1>";
-  } else {
-    results += "<h1>Possible Actions</h1>";
-    results += "<ul><li><a href='/tables'>List Tables</a></li></ul>";
-  };
-  return results + '</body>';
+  return '<body>' + body_content_method(url)() + '</body>';
 };
+
+
+var body_content_method = function(url) {
+  return url === '/tables' ? tables_body : index_body;
+};
+
+
+var tables_body = function() {
+  return '<h1>Tables</h1>';
+};
+
+
+var index_body = function() {
+  results = '<h1>Possible Actions</h1>';
+  results += '<ul><li><a href="/tables">List Tables</a></li></ul>';
+  return results;
+};
+
 
 http.createServer(function(request, response) {
   var content = html(request.url);
