536d5ccac249bd2501a01c3e3e7b9bba79202d0d
Derek Croft <derek@derekcroft.com>
2011-03-10 20:07:16 -0500

    Add tables action.

--- pit_boss.js
+++ pit_boss.js
@@ -7,15 +7,23 @@ var http = require('http'),
 
 
 http.createServer(function(request, response) {
-  var html = '<html><body>Serving Craps since 2011</body></html>';
+  var title = '<title>Serving Craps since 2011</title>';
+  var body;
+  if (request.url === '/tables') {
+    body = "<h1>Tables</h1>";
+  } else {
+    body = "<h1>Possible Actions</h1>";
+    body += "<ul><li><a href='/tables'>List Tables</a></li></ul>";
+  };
+  var html = '<html><head>' + title + '</head><body>' + body + '</body>';
   response.writeHead(200, {
     'Content-Type': 'text/html',
     'Content-Length': html.length
   });
   response.end(html);
 }).listen(8124, '127.0.0.1', function() {
-	    console.log('PitBoss started on 127.0.0.1:8124 with pid ' + process.pid);
-	  });
+  console.log('PitBoss started on 127.0.0.1:8124 with pid ' + process.pid);
+});
 
 
 process.addListener('SIGINT', function() {
@@ -23,10 +31,7 @@ process.addListener('SIGINT', function() {
   process.exit(0);
 });
 
+
 process.on('exit', function() {
   console.log('PitBoss exited');
 });
-
-
-
-
