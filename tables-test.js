0311bccc81ef1996485534baaf7b2f5b96f2ad1b
Derek Croft <derek@derekcroft.com>
2011-03-24 20:44:13 -0500

    Guess who wrote some honored VOWS.

---
+++ tables-test.js
@@ -0,0 +1,21 @@
+var vows = require('vows'),
+    assert = require('assert');
+
+var tables = require('./tables');
+
+var CrapsTable = tables.CrapsTable;
+
+vows.describe('Craps Tables').addBatch({
+  'Craps tables': {
+    'that do not exist yet': {
+      topic: new(CrapsTable),
+      'can be created': function(topic) {
+        assert.instanceOf(topic, CrapsTable);
+      } 
+    },
+    'that already exist': {
+      topic: [new(CrapsTable), new(CrapsTable)]
+    }
+  }
+}).run();
+
