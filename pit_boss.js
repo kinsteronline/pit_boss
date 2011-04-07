f4372f187c569abd5b10ab9d51242e8c1daba257
kinsteronline <todd.makinster@gmail.com>
2011-04-06 21:38:24 -0500

    A naive implementation of a webserver.
    Serving up some static assets.
    Rendering simple image using canvas.
    Using the awesome 60's style background.

--- pit_boss.js
+++ pit_boss.js
@@ -2,18 +2,71 @@
 
 require.paths.unshift(__dirname + '/static/js');
 
-var http = require('http');
-var  _ = require('./lib/underscore-min')['_'];
-var html = require('./html');
-var url = require('url');
+var http = require('http'),
+    url  = require('url'),
+    fs   = require('fs')
+    _    = require('underscore-1.1.5.min')['_']; 
 
+var html = require('./html');
+
+/* 
+ * Naive! 
+ * 
+ * 1. Make a 404/500 functions
+ * 2. Be smarter about setting content-type
+ * 3. Be smarter about parsing the path and rendering static file
+ */  
 http.createServer(function(request, response) {
-  var content = html.html(url.parse(request.url));
-  response.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Content-Length': content.length
-  });
-  response.end(content);
+  
+  function writeOutHtml(content, status, type) {
+    status = status || 200;
+    type = type || 'html';
+
+    response.writeHead(status, {
+      'Content-Type': 'text/' + type,
+      'Content-Length': content.length  
+    });
+    response.end(content);
+  }
+
+  var path = url.parse(request.url).pathname,
+      content = "";
+
+  console.log("REQ'D: " + path);
+
+  if(path == '/') {
+    fs.readFile(__dirname + '/static/table.html', function(err, data) {
+      writeOutHtml(data);
+    });
+
+  } else {
+    var re = /.+\.(html|png|css|js)$/i;
+    var requestedFile = re.exec(path);
+ 
+    if(requestedFile) {
+      var contentType = requestedFile[1];
+      requestedFile = __dirname + '/static' + requestedFile.input;
+
+      fs.stat(requestedFile, function(err, stats) {  
+        if(err) {
+          console.log('ERROR: trying to stat "' + requestedFile + '"');
+          writeOutHtml('<html><body>we just crapped out</body></html>', 500);
+        } else {
+          fs.readFile(requestedFile, function(err, data) {
+            if(err) {
+              console.log('ERROR: trying to read: ' + requestedFile);
+              writeOutHtml('<html><body>we just crapped out</body></html>', 500);
+            } else {
+              writeOutHtml(data, 200, contentType);
+            } 
+          }); 
+        }
+      });
+
+    } else {
+      writeOutHtml('<html><body>404</body></html>', 404);
+    }
+  }
 }).listen(8124, '127.0.0.1', function() {
   console.log('PitBoss started on 127.0.0.1:8124 with pid ' + process.pid);
 });
@@ -26,3 +79,4 @@ process.addListener('SIGINT', function() {
 process.on('exit', function() {
   console.log('PitBoss exited');
 });
+
