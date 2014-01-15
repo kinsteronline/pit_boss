2c79af7047c6bdca40756590b066651a5cf5efb3
kinsteronline <todd.makinster@gmail.com>
2014-01-15 11:33:50 -0600

    Use the debug package for, um, debugging.

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ const WebSocketServer = require('ws').Server,
   crapserver = require('./server/craps'),
   config = require('config');
 
+var debug = require('debug')('pitboss');
+
 var redis = require('./server/db');
 redis.on('ready', function() {
   // it would be an interesting thing to run down the list of things to
   // start using deferred or at the minimum, async
   // This would also require the inclusion of a shutdown/cleanup.
-  console.log('redis ] is ready, proceed');
+  debug('redis is ready, proceed');
 });
 
 var server = http.createServer(webserver);
 server.listen(config.webserver.port, config.webserver.host, function() {
-  console.log("PitBoss: Gamblin' is happening on port %s:%d", config.webserver.host, config.webserver.port);
+  debug("Gamblin' is happenin' on port %s:%d", config.webserver.host, config.webserver.port);
 });
 
 var websocket = new WebSocketServer({ server: server });
--- server/db.js
+++ server/db.js
@@ -1,17 +1,19 @@
 const redis = require('redis'),
       config = require('config');
 
+var debug = require('debug')('redis');
+
 var client = redis.createClient(config.redis.port, config.redis.host, config.redis);
 if(config.redis.db !== 0) client.select(config.redis.db); 
 
 module.exports = client;
 
 client.on('connect', function() {
-  console.log('redis ] connected on ' + config.redis.host + ':' + config.redis.port);
+  debug('connect on ' + config.redis.host + ':' + config.redis.port);
 });
 
 client.on('end', function() {
-  console.log('redis ] has ended');
+  debug('ended');
 });
 
 client.on('error', function(err) {
