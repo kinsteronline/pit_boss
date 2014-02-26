33dd31b435720c2a93caf2ee672cef0800c8618d
kinsteronline <todd.makinster@gmail.com>
2014-02-25 20:05:56 -0600

    Wherever things stand...

--- client/pit-boss-craps.js
+++ client/pit-boss-craps.js
@@ -1,4 +1,25 @@
-
+/*
+ * TheHouse() --> ALWAYS OPEN!!
+ *   opening, open, closing, closed
+ *
+ * CrapsTable()
+ *  closed
+ *  closing
+ *  comeOut
+ *  point
+ *  diceOut
+ *  sevenOut
+ *  payout - pay winning bets
+ *  collection - grab losing bets
+ *  pointHitPayout ??
+ *
+ * /
+ * /signin
+ * /signout
+ * /tables
+ * /table/:tid
+ * /house
+ */
 $(function() {
   'use strict';
 
@@ -32,8 +53,14 @@ $(function() {
 
   //
   // gamewebsocket
+  var msg = {
+    HELLO: 0,
+    GOODBYE: 1,
+    TABLE_LIST: 2
+  };
   var gws = new WebSocket('ws://' + window.location.host + '/');
   gws.onopen = function() {
-    gws.send('i have money to lose');
+    gws.send(msg.HELLO);
   };
+
 });
--- server/craps.js
+++ server/craps.js
@@ -1,9 +1,27 @@
 
+const events = require('events'),
+      util = require('util'),
+      _ = require('underscore');
+
+const crapsTable = require('../lib/craps_table');
+
 var redis = require('./db');
 
-var craps = function(ws) {
-  ws.on('message', function(msg) {
-    console.log('Game Msg: "' + msg + '"');
+function TheHouse() {
+  events.EventEmitter.call(this);
+}
+util.inherits(TheHouse, events.EventEmitter);
+
+var craps = function(websocket) {
+  var ws = websocket;
+
+  var tables = [];
+  if(tables.length === 0)
+    tables.push(crapsTable());
+
+  ws.on('message', function(data, flags) {
+    console.log("Binary? " + flags.binary);
+    console.log('Game Msg: "' + data  + '"');
   });
 };
 
