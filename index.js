db92084f3e1be0155448e317e87379c7d24ed096
kinsteronline <todd.makinster@gmail.com>
2017-03-07 21:47:28 -0600

    Very beginnings of a jwt protected playground.

--- index.js
+++ index.js
@@ -4,18 +4,37 @@ const port = parseInt(process.env.PORT, 10) || 2312
 
 const Koa = require('koa')
 const server = new Koa()
+const uhttp = require('uws').http
 
-server.context.redis = 'redis'
+const koajwt = require('koa-jwt')
+const jwt = koajwt({ sercret: 'this is a secret' })
 
+// connect to a datastore
+server.context.gambler = 'Detroit Slim'
+
+server.use(async (ctx, next) => {
+  if (ctx.url.match(/^\/login/)) {
+    return ctx.body = 'grab your chips'
+  }
+  await next()
+})
+
+// everything else should be protected...
+server.use(jwt)
+
+// get the degenerate gambler state
 server.use(async (ctx, next) => {
   ctx.state.chips = '$ 4954'
   await next()
 })
 
-server.use((ctx) => {
-  ctx.body = `pit boss craps: ${ctx.redis} ${ctx.state.chips}`
+// show the gambler just how broke he is
+server.use(async (ctx) => {
+  ctx.body = `pit boss craps: ${ctx.gambler} ${ctx.state.chips}`
 })
 
-server.listen(port, host, function () {
-  console.log(`started on ${host}:${port}`)
-})
+uhttp
+  .createServer(server.callback())
+  .listen(port, host, function () {
+    console.log(`started on ${host}:${port}`)
+  })
