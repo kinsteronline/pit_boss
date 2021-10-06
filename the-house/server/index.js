50e1dc1c5792f3d8a39299e5fda63cef1e1f183a
Todd Makinster <todd.makinster@gmail.com>
2021-10-06 16:17:29 -0500

    Hook up to a KeyDB with IORedis lib

--- the-house/server/index.js
+++ the-house/server/index.js
@@ -1,13 +1,24 @@
 import express from 'express'
+import IORedis from 'ioredis'
 
-const server = express()
+import debugging from 'debug'
+
 const {
   HTTP_PORT = 8080,
   HTTP_HOST = '127.0.0.1',
+  REDIS_URL = 'redis://127.0.0.1:6379',
 } = process.env
 
-server.get('/', (req, res) => {
-  res.send('The House')
+
+const debug = debugging('house:')
+const redis = new IORedis(REDIS_URL)
+const server = express()
+
+debug('House is Connected')
+
+server.get('/', async (req, res) => {
+  const str = await redis.ping('The House || Eye in the Sky')
+  res.send(str)
 })
 
 server.listen(HTTP_PORT, HTTP_HOST, () => {
