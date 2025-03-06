3149f110c38abdb37d3b747f8fea1305ba18439f
Todd Makinster <todd.makinster@gmail.com>
2025-03-06 05:31:45 -0600

    v0.0.2

--- boxman/cmd/game/main.go
+++ boxman/cmd/game/main.go
@@ -1,17 +1,67 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/charmbracelet/log"
+	nanoid "github.com/matoous/go-nanoid/v2"
 )
 
+const (
+	version = "0.0.2"
+)
+
+var (
+	debug = flag.Bool("debug", false, "Show debugging logs")
+	addr  = flag.String("addr", ":2312", "HTTP server port")
+)
+
+func serveRoot(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "405", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, fmt.Sprintf("<html><body style=\"background:#000;color:#aaa\">Boxman %s</body></html>", version))
+}
+
 func main() {
+	flag.Parse()
+
+	var loggingLevel log.Level
+	if *debug {
+		loggingLevel = log.DebugLevel
+	} else {
+		loggingLevel = log.Default().GetLevel()
+	}
+
 	logger := log.NewWithOptions(os.Stderr, log.Options{
+		ReportCaller:    true,
 		ReportTimestamp: false,
 		TimeFormat:      time.Kitchen,
+		Level:           loggingLevel,
 	})
 
-	logger.Info("PitBoss: Boxman version 0.0.1")
+	id, err := nanoid.New(11)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger.Info(fmt.Sprintf("PitBoss ⚀ Boxman ⚁ Ver.%s", version))
+	logger.Info("Session", "id", id)
+	logger.Debug("Debugging this mess")
+
+	http.HandleFunc("/", serveRoot)
+	logger.Fatal(http.ListenAndServe(*addr, nil))
 }
--- boxman/go.mod
+++ boxman/go.mod
@@ -2,13 +2,14 @@ module kinster.com/boxman
 
 go 1.24
 
+require github.com/charmbracelet/log v0.4.0
+
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
-	github.com/charmbracelet/log v0.4.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/matoous/go-nanoid/v2 v2.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
